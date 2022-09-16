9c720b0e26eeb5b080cc72f1ba33ac0378ca03e6
Nokestro <Nokestro@yandex.by>
2022-09-16 14:22:58 +0300

    Texted Hello

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-"Hello"
+World = None
+Name = "Ilya"
+print (Name)
+a = 156
+print (a)
+b = 1689.1
+print (b)
+c = True
+surname = "Kishcunov"
+D = 26
+print (D)
+d = 25.5
+print(D-d)
+print (Name, surname)
+A = True
+B = False
+print (type(A))
+print (B)
+Void = None
+print (Void)
+print ("World is", World)
